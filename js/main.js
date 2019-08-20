be3c8cb42bbb6d483cf11e3f6ed0e1f330a67718
LukasMeier <lukas.meier94@gmail.com>
2019-08-20 10:59:28 +0200

    Improved frontend further

--- js/main.js
+++ js/main.js
@@ -24,36 +24,56 @@ import Lumens from "./lumens"
 })([Element.prototype, Document.prototype, DocumentFragment.prototype])
 
 window.addEventListener("load", () => {
-
   window.default = new Lumens(".defaultslider")
 
   window.autoplay = new Lumens(".autoplayslider", {
     autoplay: 2500
   })
 
-  window.a = new Lumens(
-    ".productslider",
-    {
-      slidesPerPage: 2,
-      margin: 10,
-      draggable: true,
-      responsive: [
-        {
-          breakpoint: 600,
-          settings: {
-            slidesPerPage: 1,
-            margin: 0
-          }
+  window.preventclick = new Lumens(".productslider")
+
+  window.callbackslider = new Lumens(".callbackslider", {
+    slidesPerPage: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesPerPage: 1
         }
-      ]
-    },
-    true
-  )
+      }
+    ]
+  })
+
+  window.callbackslider.changed(() => {
+    document.getElementById("callback").innerHTML = "Slide Changed!"
+    setTimeout(() => {
+      document.getElementById("callback").innerHTML = "&nbsp;"
+    }, 500)
+  })
+
+  window.callbackslider.resize(() => {
+    document.getElementById("callback").innerHTML = "New Breakpoint reached"
+    setTimeout(() => {
+      document.getElementById("callback").innerHTML = "&nbsp;"
+    }, 1000)
+  })
 
   let slides = document.querySelectorAll(".slide img")
   Array.from(slides).forEach(slide => {
     slide.addEventListener("click", () => {
       window.location.href = "https://placekitten.com"
     })
+  })
+
+  window.apislider = new Lumens(".apislider", {
+    draggable: false
+  })
+
+  document.getElementById("next").addEventListener("click", () => {
+    window.apislider.gotoNext()
+  })
+
+  document.getElementById("prev").addEventListener("click", () => {
+    window.apislider.gotoPrev()
   })
 })
